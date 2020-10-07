20832e74e99081a8c976d2c369c6b0df2e138dac
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-10-07 15:45:13 +0800

    Add footer.php

---
+++ footer.php
@@ -0,0 +1,78 @@
+<footer>
+       <div class="row">
+        <div class="column small-12 medium-3">
+          <h4>Departments</h4>
+          <ul class="no-bullet">
+            <li><a href="#">Aviation</a></li>
+            <li><a href="#">land</a></li>
+            <li><a href="#">Maritime</a></li>
+            <li><a href="#">Logistics</a></li>
+          </ul>
+        </div>
+        <div class="column small-12 medium-3">
+          <h4>Topics</h4>
+          <ul class="no-bullet">
+            <li><a href="#">Infrastructure</a></li>
+            <li><a href="#">Safety</a></li>
+            <li><a href="#">Operators & License</a></li>
+            <li><a href="#">Development</a></li>
+            <li><a href="#">Agencies</a></li>
+            <li><a href="#">Acts and Regulations</a></li>
+            <li><a href="#">Reports and Stats</a></li>
+            <li><a href="#">Programme</a></li>
+          </ul>
+        </div>
+        <div class="column small-12 medium-3">
+          <h4>Contact Us</h4>
+          <ul class="no-bullet">
+            <li><img src="assets/icon/phone-receiver.svg" alt="phone-icon"> <a href="#">03-8000 8000</a></li>
+            <li><img src="assets/icon/printer-.svg" alt="printer-icon"> <a href="#">03-8888 0158</a></li>
+            <li><img src="assets/icon/close-envelope.svg" alt="mail-icon"> <a href="#">aduan@mot.gov.my</a></li>
+            <li><a href="#">Staff Directory</a></li>
+            <li><a href="#">Aduan</a></li>
+          </ul>
+        </div>
+        <div class="column small-12 medium-3 mot-info">
+          <img src="assets/icon/mot-logo.svg" alt="mot-logo" id="mot-logo">
+          <h4>Ministry of Transport Malaysia</h4>
+          <p>No. 26, Jalan Tun Hussein, Presint 4, 62100 W.P. Putrajaya</p>
+          <ul class="no-bullet social-links" >
+            <li><a href="#"><img src="assets/icon/facebook.svg" alt="fb-icon"></a></li>
+            <li><a href="#"><img src="assets/icon/instagram.svg" alt="insta-icon"></a></li>
+            <li><a href="#"><img src="assets/icon/twitter.svg" alt="twitter-icon"></a></li>
+            <li><a href="#"><img src="assets/icon/youtube.svg" alt="youtube-icon"></a></li>
+            <li><a href="#"><img src="assets/icon/rss.svg" alt="-icon"></a></li>
+          </ul>
+        </div>
+        </div>
+      <div class="grid-container footer-quick-links ">
+        <div class="row">
+          <div class="column small-12">
+            <ul class="no-bullet">
+              <li><a href="#">Home</a></li> |
+              <li><a href="#">Site Map</a></li> |
+              <li><a href="#">Security Policy</a></li> |
+              <li><a href="#">Privacy Policy</a></li> |
+              <li><a href="#">Disclaimer</a></li> |
+              <li><a href="#">Freedom of Information</a></li> |
+              <li><a href="#">FAQ's</a></li>
+          </ul>
+          </div>
+      </div>
+    </footer>
+        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>
+        <script src="js/slick.js"></script>
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/vendor/what-input.js"></script>
+        <script src="js/vendor/foundation.js"></script>
+        <script src="js/app.js"></script>
+        <script src="js/main.js"></script>
+        <!-- <script>
+          $('.icon').click(function(){
+            $('span').toggleClass("cancel");
+          });
+        </script> -->
+  </body>
+</html>
