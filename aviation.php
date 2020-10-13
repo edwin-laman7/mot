84f12aa60a98820abd07a51ddbab41fef9337663
aisyahrozi <72720093+aisyahrozi@users.noreply.github.com>
2020-10-13 15:30:43 +0800

    Internal page: Breadcrumb, Page Banner
    Internal page template with sidebar
    + Added links to header, and index.php

---
+++ aviation.php
@@ -0,0 +1,101 @@
+<?php include('header.php'); ?>
+<main>
+  <!-- Internal Page header -->
+       <div class="grid-container page-banner">
+        <div class="row">
+          <div class="column small-12">
+           <h1>Internal page</h1>
+          </div>
+        </div>
+      </div>
+
+  <!-- Page Template: Content with Sidebar
+       Page Main Content -->
+
+  <div class="grid-container">
+    <div class="row">
+      <div class="column small-12 medium-8">
+
+        <nav aria-label="You are here:" role="navigation">
+          <ul class="breadcrumbs">
+            <li><a href="#" class="home">Home</a></li>
+            <li><a href="#">Aviation</a></li>
+              <span class="show-for-sr">Current: </span> Aviation Infrastructure
+            </li>
+          </ul>
+      </nav>
+        <h2> Aviation Infrastructure </h2>
+
+        <p>The overall infrastructure of aviation in Malaysia, governed by MOT. Repudiandae voluptatem corrupti in. Voluptas occaecati corrupti mollitia fugit minima voluptatem qui. Excepturi id ex voluptate fugit. Dolores sit facilis.</p>
+
+        <p>The overall infrastructure of aviation in Malaysia, governed by MOT. Repudiandae voluptatem corrupti in. Voluptas occaecati corrupti mollitia fugit minima voluptatem qui. Excepturi id ex voluptate fugit. Dolores sit facilis.</p>
+
+        <p>The overall infrastructure of aviation in Malaysia, governed by MOT. Repudiandae voluptatem corrupti in. Voluptas occaecati corrupti mollitia fugit minima voluptatem qui. Excepturi id ex voluptate fugit. Dolores sit facilis.</p>
+
+        <p>The overall infrastructure of aviation in Malaysia, governed by MOT. Repudiandae voluptatem corrupti in. Voluptas occaecati corrupti mollitia fugit minima voluptatem qui. Excepturi id ex voluptate fugit. Dolores sit facilis.</p>
+
+        <p>The overall infrastructure of aviation in Malaysia, governed by MOT. Repudiandae voluptatem corrupti in. Voluptas occaecati corrupti mollitia fugit minima voluptatem qui. Excepturi id ex voluptate fugit. Dolores sit facilis.</p>
+
+        <table class="unstriped">
+          <thead>
+            <tr>
+              <th width="200">Table Header</th>
+              <th>Table Header</th>
+              <th width="150">Table Header</th>
+              <th width="150">Table Header</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Content Goes Here</td>
+              <td>This is longer content Donec id elit non mi porta gravida at eget metus.</td>
+              <td>Content Goes Here</td>
+              <td>Content Goes Here</td>
+            </tr>
+            <tr>
+              <td>Content Goes Here</td>
+              <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+              <td>Content Goes Here</td>
+              <td>Content Goes Here</td>
+            </tr>
+            <tr>
+              <td>Content Goes Here</td>
+              <td>This is longer Content Goes Here Donec id elit non mi porta gravida at eget metus.</td>
+              <td>Content Goes Here</td>
+              <td>Content Goes Here</td>
+            </tr>
+          </tbody>
+        </table>
+
+      </div>
+
+<!-- Sidebar Content -->
+
+      <div class="column small-12 medium-4">
+        
+        <div class="sidebar-wrap sticky">
+          <h3>Aviation by Topic</h3>
+          <nav>
+            <ul class="sidebar-nav">
+            <li><a href="#">Infrastructure</a></li>
+            <li><a href="#">Safety and Security</a></li>
+            <li><a href="#">Acts and Regulation</a></li>
+            <li><a href="#">Development</a></li>
+            <li><a href="#">Programmes</a></li>
+            <li><a href="#">Operators & Licensing</a></li>
+            <li><a href="#">Related Agencies</a></li>
+            <li><a href="#">Reports and Stats</a></li>
+          </ul>
+          </nav>
+
+        </div>
+
+
+      </div>
+
+
+  </div>
+   
+</main>
+
+<?php include('footer.php'); ?>
