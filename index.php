3da80c76693d30b4d758cc8aa7d1d1b089351e62
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-10-14 15:15:57 +0800

    Update Style

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
          <div class="column small-12 medium-12">
             <ul class="main-sector-btn">
               <li class="one"><a href="aviation.php"><img src="assets/icon/plane.svg" alt="plane-icon"> Aviation</a></li>
-              <li class="two"><a href="#"><img src="assets/icon/train.svg" alt="train-icon"> <img src="assets/icon/bus.svg" alt="bus-icon"> Land</a></li>
+              <li class="two"><a href="#"><img src="assets/icon/train.svg" alt="train-icon"> Land</a></li>
               <li class="three"><a href="#"><img src="assets/icon/ship.svg" alt="ship-icon"> Maritime</a></li>
               <li class="four"><a href="#"><img src="assets/icon/box.svg" alt="box-icon"> Logistic</a></li>
             </ul>
