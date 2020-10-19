f411ff08b8f829c862a287127df5da79b975b881
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-10-19 15:27:36 +0800

    Add index2.php

---
+++ index2.php
@@ -0,0 +1,222 @@
+<?php include('header.php'); ?>
+
+<main>
+    <!-- Main Sector Code-->
+    <div class="grid-container">
+        <div class="row">
+            <div class="column small-12">
+                <div class="main-sector-caption">
+                    <h3>Moving people and goods safely, efficiently and sustainably accross Malaysia to improve quality of life and support a competitive economy.</h3>
+                </div>
+                <div class="main-sector-button">
+                    <button>Aviation</button>
+                    <button>Land</button>
+                    <button>Maritime</button>
+                    <button>Logistic</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Banner Announcement Code -->
+    <div class="grid-container banner-announcements" > 
+        <div class="row">
+          <div class="column text-center">
+            <div class="responsive">
+              <div class="column">
+                <button>
+                  <a href="assets/img/banci-malaysia.jpg">
+                    <img src="assets/img/banci-malaysia.jpg" alt="Banci">
+                  </a>
+                    <span class="banner-announcement-caption">Bancian Penduduk dan Perumahan Malaysia</span>
+                </button>
+              </div>
+              <div class="column">
+                <button>
+                  <a href="assets/img/63-years-malaysia.jpg">
+                    <img src="assets/img/63-years-malaysia.jpg" alt="Jalur Gemilang - 63">  
+                  </a>
+                  <span class="banner-announcement-caption">63 Years Malaysia Celebration</span>
+                </button>
+              </div>
+              <div class="column">
+                <button>
+                  <a href="assets/img/covid-19.jpg">
+                    <img src="assets/img/covid-19.jpg" alt="Covid"">
+                  </a>
+                  <span class="banner-announcement-caption">Covid-19 Updates</span>
+                </button>
+              </div>
+              <div class="column">
+                <button>
+                  <a href="assets/img/63-years-malaysia.jpg">
+                    <img src="assets/img/63-years-malaysia.jpg" alt="Jalur Gemilang - 63">  
+                  </a>
+                  <span class="banner-announcement-caption">63 Years Malaysia Celebration</span>
+                </button>
+              </div>
+              <div class="column">
+                <button>
+                  <a href="assets/img/covid-19.jpg">
+                    <img src="assets/img/covid-19.jpg" alt="Covid"">
+                  </a>
+                  <span class="banner-announcement-caption">Covid-19 Updates</span>
+                </button>
+              </div>
+              <div class="column">
+                <button>
+                  <a href="assets/img/63-years-malaysia.jpg">
+                    <img src="assets/img/63-years-malaysia.jpg" alt="Jalur Gemilang - 63">  
+                  </a>
+                  <span class="banner-announcement-caption">63 Years Malaysia Celebration</span>
+                </button>
+              </div>
+              <div class="column">
+                <button>
+                  <a href="assets/img/dummy-banner.jpg">
+                    <img src="assets/img/dummy-banner.jpg" alt="Covid"">
+                  </a>
+                  <span class="banner-announcement-caption">Covid-19 Updates</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+    <!-- Front Topic Code -->
+    <div class="grid-container">
+        <div class="row">
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/infrastructure-icon.png" alt="infrastruture-icon">
+                <h2><a href="#">Infrastruture</a></h2>
+                <p>Ready transportation Infrastruture in Malaysia.</p>
+            </div>
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/safety-and-security-icon.png" alt="safetyAndSecurity-icon">
+                <h2><a href="#">Safety and Security</a></h2>
+                <p>Protect against internal and external transportation threats.</p>
+            </div>
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/operator-and-license-icon.png" alt="operatorAndLicense-icon">
+                <h2><a href="#">Operator And License</a></h2>
+                <p>Available applicable transportation operator license.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/development-icon.png" alt="development-icon">
+                <h2><a href="#">Development</a></h2>
+                <p>New and ongoing transportation projects across Malaysia.</p>
+            </div>
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/agencies-icon.png" alt="agencies-icon">
+                <h2><a href="#">Agencies</a></h2>
+                <p>Enforcement Agencies under Transportation Ministry</p>
+            </div>
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/acts-and-regulation-icon.png" alt="actsAndRegulation-icon">
+                <h2><a href="#">Acts and Regulation</a></h2>
+                <p>Transportation Law governing Air, Land and Water.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/reports-and-stats-icon.png" alt="reportsAndStats-icon">
+                <h2><a href="#">Reports and Stats</a></h2>
+                <p>Official Transportation Reports and Statistics.</p>
+            </div>
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/programmes-icon.png" alt="programmes-icon">
+                <h2><a href="#">Programmes</a></h2>
+                <p>Programmes to help transportation businesses and individuals.</p>
+            </div>
+            <div class="column small-12 medium-4">
+                <img src="assets/img/index2/feedback-and-complaint-icon.png" alt="feedbackAndComplaint-icon">
+                <h2><a href="#">Feedback and Complaint</a></h2>
+                <p>Provide feedback or complaints to improve transportation policies.</p>
+            </div>
+        </div>
+    </div>
+    <!-- News and Development Code -->
+    <div class="grid-container mot-news-developments" style="background-color:#F1F3F6;">
+        <div class="row">
+          <div class="column small-12 medium-6">
+            <h2>MOT in the News</h2>
+            <ul class="no-bullet">
+              <li><span class="date">20/09</span> <a href="#"> Govt Gets Tough On Traffic Offences</a></li>
+              <li><span class="date">20/09</span> <a href="#"> JB - S'PORE RTS Expected To Run By 2026</a></li>
+              <li><span class="date">20/09</span> <a href="#"> RTS Project To Be inked On July 30</a></li>
+              <li><span class="date">20/09</span> <a href="#"> M'sia Vows To Seek Closure for MH17 families</a></li>
+            </ul>
+            <a href="#">More News</a>
+          </div>
+          <div class="column small-12 medium-6">
+            <h2>Future Developments</h2>
+            <ul class="no-bullet">
+              <li><a href="#">MRT and LRT3 Project Status</a></li>
+              <li><a href="#">Klang Valley Dual Track 2 (KVDT2)</a></li>
+              <li><a href="#">Malaysia-Singapore Rapid Transit System (RTS)</a></li>
+              <li><a href="#">Special Report Air Investigation</a></li>
+            </ul>
+            <a href="#">More Developments</a>
+          </div>
+        </div>
+      </div>
+      <!-- Agencies Under Mot Code -->
+      <div class="grid-container agencies-under-mot">
+        <div class="row">
+          <div class="column small-12 medium-12">
+            <h2>Agencies Under MOT</h2>
+          </div>
+          <div class="columns small-12 medium-4 aviation-agencies">
+            <h3>Aviation</h3>
+            <ul class="no-bullet">
+              <li>
+                <span>Flight safety, air traffic controller, flight school, pilot license.</span>
+                <p><a href="#">Civil Aviation Authority Malaysia (DCA)</a></p>
+              </li>
+              <li>
+                <span>Public air transportation, flight refund, air traffic rights, aviation economic.</span>
+                <p><a href="#">Malaysia Aviation Commission (MAVCOM)</a></p>
+              </li>
+            </ul>
+          </div>
+          <div class="columns small-12 medium-4 land-logistics-agencies">
+            <h3>Land and Logistics</h3>
+            <ul class="no-bullet">
+              <li>
+                <span>Vehicle registration, driving licenses, summons, road safety.</span>
+                <p><a href="#">Road Transport Department (JPJ)</a></p>
+              </li>
+              <li>
+                <span>e-Hailing, LRT, MRT, train, bus, taxi transport matters.</span>
+                <p><a href="#">Land Public Transport Agency (APAD)</a></p>
+              </li>
+              <li>
+                <span>Road and public transportation safety.</span>
+                <p><a href="#">Land Public Transport Agency (APAD)</a></p>
+              </li>
+            </ul>
+          </div>
+          <div class="columns small-12 medium-4 maritime-agencies">
+            <h3>Maritime</h3>
+            <ul class="no-bullet">
+              <li>
+                <span>Marine, shipping, ferry, port policy and marine notices.</span>
+                <p><a href="#">Marine Department Malaysia (MDM)</a></p>
+              </li>
+              <li>
+                <span>Ocean law, Malacca Straits, Sea security/safety, marine industries</span>
+                <p><a href="#">Maritime Institute of Malaysia (MIMA)</a></p>
+              </li>
+              <li>
+                <span>Port authorities, Port Klang, Johor Port, Kuantan Port, Bintulu Port, Penang</span>
+                <p><a href="#">All port authorities</a></p>
+              </li>
+            </ul>
+          </div>
+      </div>
+    </div>
+</main>
+
+
+<?php include('footer.php'); ?>
