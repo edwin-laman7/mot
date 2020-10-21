2244f4a850a4665b38f776ec0d16f003846084c7
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-10-21 16:36:17 +0800

    Delete unnecessary files

--- layout-boxed-content-bottom-2.php
+++ layout-boxed-content-bottom-2.php
@@ -1,5 +1,5 @@
 <?php include('header.php'); ?>
-  <main class="boxed-bottom-2">
+  <main class="boxed-content-bottom-2">
     <div class="grid-container breadcrumb-wrap">
       <div class="row">
           <div class="columns small-12 large-12">
@@ -17,110 +17,106 @@
       </div>
     </div>
     <div class="staff-directory">
-      <div class="grid-container">
-            <div class="row">
-                <div class="column small-12 large-4 desktop-first-column">
-                  <h3><a href="#">Aviation Division</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Logistic and Land Transport</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Maritime Division</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-            </div>
+    <div class="grid-container">
+          <div class="row">
+              <div class="column small-12 large-4 desktop-first-column">
+                <h3><a href="#">Aviation Division</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Logistic and Land Transport</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Maritime Division</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
           </div>
-          <div class="grid-container">
-            <div class="row">
-                <div class="column small-12 large-4 desktop-first-column">
-                  <h3><a href="#">Minister's Office</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Deputy Minister's Office</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Secretary General Office</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-            </div>
+        </div>
+        <div class="grid-container">
+          <div class="row">
+              <div class="column small-12 large-4 desktop-first-column">
+                <h3><a href="#">Minister's Office</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Deputy Minister's Office</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Secretary General Office</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
           </div>
-          <div class="grid-container">
-            <div class="row">
-                <div class="column small-12 large-4 desktop-first-column">
-                  <h3><a href="#">Human Resource Management</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Administration and Finance</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Account</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-            </div>
+        </div>
+        <div class="grid-container">
+          <div class="row">
+              <div class="column small-12 large-4 desktop-first-column">
+                <h3><a href="#">Human Resource Management</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Administration and Finance</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Account</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
           </div>
-          <div class="grid-container">
-            <div class="row">
-                <div class="column small-12 large-4 desktop-first-column">
-                  <h3><a href="#">Strategy Planning and International</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Development Division</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Information Management</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-            </div>
+        </div>
+        <div class="grid-container">
+          <div class="row">
+              <div class="column small-12 large-4 desktop-first-column">
+                <h3><a href="#">Strategy Planning and International</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Development Division</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Information Management</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
           </div>
-          <div class="grid-container">
-            <div class="row">
-                <div class="column small-12 large-4 desktop-first-column">
-                  <h3><a href="#">Corporate Communication Unit</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Legal Advisor's Office</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4">
-                  <h3><a href="#">Internal Audit</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-            </div>
+        </div>
+        <div class="grid-container">
+          <div class="row">
+              <div class="column small-12 large-4 desktop-first-column">
+                <h3><a href="#">Corporate Communication Unit</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Legal Advisor's Office</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4">
+                <h3><a href="#">Internal Audit</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
           </div>
-          <div class="grid-container">
-            <div class="row">
-                <div class="column small-12 large-4 desktop-first-column">
-                  <h3><a href="#">Integrity Unit</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4 desktop-second-column">
-                  <h3><a href="#">KPI Unit</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-                <div class="column small-12 large-4 desktop-third-column">
-                  <h3><a href="#">Air Accident Investigation Bureau</a></h3>
-                  <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
-                </div>
-            </div>
-          </div> 
+        </div>
+        <div class="grid-container">
+          <div class="row">
+              <div class="column small-12 large-4 desktop-first-column">
+                <h3><a href="#">Integrity Unit</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4 desktop-second-column">
+                <h3><a href="#">KPI Unit</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+              <div class="column small-12 large-4 desktop-third-column">
+                <h3><a href="#">Air Accident Investigation Bureau</a></h3>
+                <p>Repudiandae voluptatem corrupti, Voluptas occaecati corrupti</p>
+              </div>
+          </div>
+        </div> 
+        </div>
       </div>
-    </div>
-    <div class="grid container">
-      <div class="row align-center">
-        <div class="column small-12 medium-6">Content 1</div>
-        <div class="column small-12 medium-6">Content 2</div>
-      </div>
-    </div>
+
+
   </main>
 <?php include('footer.php'); ?>
 
