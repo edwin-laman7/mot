dda3fac094058f03700c417fb3bc9daf37197137
aisyahrozi <72720093+aisyahrozi@users.noreply.github.com>
2020-10-28 19:48:53 +0800

    TOC & Aviation.php

--- aviation.php
+++ aviation.php
@@ -1,7 +1,7 @@
 <?php include('header.php'); ?>
 
 <!-- Page Template: No Sidebar, must include class "no-sidebar" in main  --> 
-<main class="no-sidebar">
+<main class="boxed">
   <!-- Internal Page header -->
        <div class="grid-container page-banner">
         <div class="row">
@@ -11,7 +11,6 @@
         </div>
       </div>
 
- 
 
   <div class="grid-container breadcrumb-wrap">
     <div class="row">
@@ -32,9 +31,7 @@
             <div class="row">
                 <div class="columns small-12 medium-8">
                     <h1>Aviation</h1>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT. Repudiandae 
-                        voluptatem corrupti in. Voluptas occaecati corrupti mollitia fugit minima voluptatem qui. 
-                        Excepturi id ex voluptate fugit. Dolores sit facilis.</p>
+                    <p>The Aviation Division is a division within the Ministry of Transport Malaysia. This division is responsible for all civil aviation affairs in Malaysia and is made up of six (6) units comprising of Air Transport, Airport Services, Hubbing & Aerospace Industry, Licensing & Regional Cooperation, Rural Air Services, and Safety & Convention.</p>
                 </div>
             </div>
         </div>
@@ -51,7 +48,7 @@
                 </div>
                 <div class="column small-12 large-4">
                     <h2>Safety and Security</h2>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
+                    <p>Policies regarding air safety standards.</p>
                     <ul class="no-bullet">
                         <li><a href="#">Overview Safety and Security</a></li>
                         <li><a href="#">Airline Safety and Security</a></li>
@@ -63,10 +60,12 @@
                 </div>
                 <div class="column small-12 large-4">
                     <h2>Operator and License</h2>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
+                    <p>Services performed by any airlines to transport passengers or cargo, known as Flight Services.​</p>
                     <ul class="no-bullet">
-                        <li><a href="#">Operators</a></li>
-                        <li><a href="#">Licenses</a></li>
+                        <li><a href="#">Scheduled</a></li>
+                        <li><a href="#">Charter</a></li>
+                        <li><a href="#">Air Services Agreement</a></li>
+                        <li><a href="#">Rural Air Servies (RAS)</a></li>
                     </ul>
                 </div>
             </div>
@@ -75,25 +74,27 @@
             <div class="row row-line">
                 <div class="column small-12 large-4">
                     <h2>Development</h2>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
+                    <p>MOT has embarked on several development projects for Air Sector. Among the projects are:</p>
                     <ul class="no-bullet">
-                        <li><a href="#">Development Project of NKRA</a></li>
-                        <li><a href="#">Development Project of Rails</a></li>
+                        <li><a href="#">Sibu Airport</a></li>
+                        <li><a href="#">Ipoh Airport</a></li>
+                        <li><a href="#">Sandakan Airport</a></li>
                     </ul>
                 </div>
                 <div class="column small-12 large-4">
                     <h2>Agencies</h2>
                     <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
                     <ul class="no-bullet">
-                        <li><a href="#">CAAM</a></li>
-                        <li><a href="#">MAVCOM</a></li>
+                        <li><a href="#">Air Accident Investigation Bureau (AAIB)</a></li>
+                        <li><a href="#">International Civil Aviation Organization (ICAO)</a></li>
+                        <li><a href="#">International Civil Aviation Organization Convention(ICAOC)</a></li>
                     </ul>
                 </div>
                 <div class="column small-12 large-4">
                     <h2>Acts and Regulation</h2>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
+                    <p>Regulated for the airlines, airports services operations and civil aviation activities.</p>
                     <ul class="no-bullet">
-                        <li><a href="#">Aviation Transport Act 19xx</a></li>
+                        <li><a href="#">Acts and Regulation</a></li>
                     </ul>
                 </div>
             </div>
@@ -102,27 +103,30 @@
             <div class="row row-line">
                 <div class="column small-12 large-4">
                     <h2>Reports and Stats</h2>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
+                    <p>Resources on reports and statistics under Ministry of Transport</p>
                     <ul class="no-bullet">
                         <li><a href="#">Air Accident Investigation</a></li>
                         <li><a href="#">MH370</a></li>
                         <li><a href="#">MH17</a></li>
                         <li><a href="#">Eurocopter AS365N3 (9M-IGB)</a></li>
+                        <li><a href="#">Quarterly Statistics of Transport</a></li>
                     </ul>
                 </div>
                 <div class="column small-12 large-4">
                     <h2>Programme</h2>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
+                    <p>Available programme, incentives and human capital development governed by MOT.</p>
                     <ul class="no-bullet">
-                        <li><a href="#">Flight School</a></li>
-                        <li><a href="#">Air Traffic Controller Training (MAYA)</a></li>
+                        <li><a href="#">Training Institutions MAvA</a></li>
+                        <li><a href="#">Flying School</a></li>
+                        <li><a href="#">Mycair</a></li>
                     </ul>
                 </div>
                 <div class="column small-12 large-4">
                     <h2>Feedback</h2>
-                    <p>The overall infrastructure of aviation in Malaysia, governed by MOT.</p>
+                    <p>Send your feedback or contact us for help and enquiries. </p>
                     <ul class="no-bullet">
                         <li><a href="#">Send your Feedback</a></li>
+                        <li><a href="#">Staff Directory</a></li>
                     </ul>
                 </div>
             </div>
