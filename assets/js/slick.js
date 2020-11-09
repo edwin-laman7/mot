deeabd62009a24323447c9215b29c4683207557d
edwinmasripan <edwin@laman7.com>
2020-11-09 18:00:19 +0800

    Update slick.js

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -27,6 +27,15 @@ $('.responsive').slick({
         {
         breakpoint: 1024,
         settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            infinite: true,
+            dots: true
+        }
+        },
+        {
+        breakpoint: 800,
+        settings: {
             slidesToShow: 1,
             slidesToScroll: 1,
             infinite: true,
