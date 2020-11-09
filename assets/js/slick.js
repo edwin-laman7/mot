257e87ebf4668fb716e9f81fba067a48c836c54d
edwinmasripan <edwin@laman7.com>
2020-11-09 17:54:56 +0800

    Update slick.js

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -21,8 +21,8 @@ $('.responsive').slick({
     dots: true,
     infinite: false,
     speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 1,
+    slidesToScroll: 1,
     responsive: [
         {
         breakpoint: 1024,
