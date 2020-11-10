83d2d4532994ed4605ef154e602a7ea27c1000fc
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-11-10 16:21:50 +0800

    Create sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,21 @@
+<?php include('header.php'); ?>
+<main class="boxed">
+<div class="grid-container">
+	<div class="row"> 
+		<div class="columns small-12 medium-12">
+<!-- do not copy above this line -->
+      <div class="row">
+        <div class="small-12 medium-4">
+          <h2><a href="#internal">About MOT</a></h2>
+          <ul>
+            <li><a href="#internal">Introduction</a></li>
+          </ul>
+        </div>
+      </div>
+<!-- do not copy BELOW this line -->
+		</div>
+	</div>
+ </div>
+</main>
+<?php include('footer.php'); ?>
+
