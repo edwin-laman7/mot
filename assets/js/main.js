5fd2c3a2319220438b180456a68433550dcbaa80
edwinmasripan <edwin@laman7.com>
2020-11-10 11:21:07 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,16 +203,16 @@ $( document ).ready(function() {
 
 
 
-//LIGHTBOX FOR BANNER ANNOUNCEMENT CODE
-// $( document ).ready(function() {
-//   $('.banner-announcements').magnificPopup({
-//     delegate: 'a.popup',
-//     type: 'image',
-//     gallery:{
-//       enabled:true
-//     }
-//   });
-// });
+LIGHTBOX FOR BANNER ANNOUNCEMENT CODE
+$( document ).ready(function() {
+  $('.banner-announcements').magnificPopup({
+    delegate: 'a.popup',
+    type: 'image',
+    gallery:{
+      enabled:true
+    }
+  });
+});
 
 $(document).ready(function() {
   $('.light-box').magnificPopup({
