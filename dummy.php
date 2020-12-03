c278caf02b9092b52a5d9d72e8b20cc35ab77784
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-12-03 11:53:50 +0800

    Update dummy.php

--- dummy.php
+++ dummy.php
@@ -1,45 +1,71 @@
 <?php include('header.php'); ?>
 <div class="row">
-
-<div class="column text-center">  
-  <div class="responsive">
-    
-    <div class="column">
-      <button>
-        <a class="popup" href="assets/img/mosti_dubai.jpg" title="Expo 2020 Dubai UAE">
-          <img src="assets/img/mosti_dubai.jpg" alt="Mosti Dubai">
-        </a>   
-      <a href="https://www.malaysiaexpo2020.com/" target="_blank"><span class="banner-announcement-caption">Expo 2020 Dubai UAE</span></a>
-      </button>
+  <div class="column text-center">  
+    <div class="responsive">
+      <div class="column">
+        <button>
+          <a class="popup" href="a/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-1.jpg">
+            <img src="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-1.jpg" alt="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-1.jpg">
+          </a>   
+        </button>
+      </div>
+      <div class="column">
+        <button>
+          <a class="popup" href="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-2.jpg">
+            <img src="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-2.jpg" alt="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-2.jpg">  
+          </a>
+        </button>
+      </div>
+      <div class="column">
+        <button>
+          <a  class="popup" href="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-3.jpg">
+            <img src="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-3.jpg" alt="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-3.jpg">
+          </a>
+        </button>
+      </div>
+      <div class="column">
+        <button>
+          <a  class="popup" href="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-4.jpg">
+            <img src="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-4.jpg" alt="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-4.jpg">
+          </a>
+        </button>
+      </div>
     </div>
-
-    <div class="column">
-      <button>
-        <a class="popup" href="assets/img/e-banci.png" title="Banci Malaysia">
-          <img src="assets/img/e-banci.png" alt="Banci Malaysia">  
-        </a>
-        <a href="https://www.mycensus.gov.my/" target="_blank"><span class="banner-announcement-caption">Banci Malaysia</span></a>
-      </button>
-    </div>
-    <div class="column">
-      <button>
-        <a  class="popup" href="assets/img/banner_baru_covidMOT_17062020.jpg" title="Covid-19 Updates">
-          <img src="assets/img/banner_baru_covidMOT_17062020.jpg" alt="Covid-19 Updates">
-        </a>
-        <a href="http://cccovid19.mot.gov.my/" target="_blank"><span class="banner-announcement-caption">Covid-19 Updates</span></a>
-      </button>
-    </div>
-    <div class="column">
-      <button>
-        <a  class="popup" href="assets/img/banner_baru_covidMOT_17062020.jpg" title="Covid-19 Updates">
-          <img src="assets/img/banner_baru_covidMOT_17062020.jpg" alt="Covid-19 Updates">
-        </a>
-        <a href="http://cccovid19.mot.gov.my/" target="_blank"><span class="banner-announcement-caption">Covid-19 Updates</span></a>
-      </button>
-    </div>
-    
   </div>
 </div>
-</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="responsive slick-initialized slick-slider slick-dotted">
+                  <button class="slick-prev slick-arrow slick-disabled" aria-label="Previous" type="button" aria-disabled="true" style="display: block;">Previous</button> 
+                  <div class="slick-list draggable">
+                     <div class="slick-track" style="opacity: 1; width: 1824px; transform: translate3d(0px, 0px, 0px);">
+                        <img src="" alt="2-sept-1.jpg" class="slick-slide slick-current slick-active" data-slick-index="0" aria-hidden="false" role="tabpanel" id="slick-slide00" aria-describedby="slick-slide-control00" style="width: 456px;"/>
+                        <img src="" alt="2-sept-2.jpg" class="slick-slide slick-active" data-slick-index="1" aria-hidden="false" role="tabpanel" id="slick-slide01" style="width: 456px;"/>
+                        <img src="" alt="2-sept-3.jpg" class="slick-slide slick-active" data-slick-index="2" aria-hidden="false" role="tabpanel" id="slick-slide02" style="width: 456px;"/>
+                        <img src="/en/PublishingImages/Pages/Land/New%20Development/National-Rail-Centre-of-Excellence/2-sept-4.jpg" alt="2-sept-4.jpg" class="slick-slide" data-slick-index="3" aria-hidden="true" tabindex="-1" role="tabpanel" id="slick-slide03" aria-describedby="slick-slide-control01" style="width: 456px;"/>&#160;</div>&#160;</div> ​ 
+                  <button class="slick-next slick-arrow" aria-label="Next" type="button" aria-disabled="false" style="display: block;">Next</button>
+                  <ul class="slick-dots" role="tablist" style="display: block;">
+                     <li class="slick-active" role="presentation">
+                        <button type="button" role="tab" id="slick-slide-control00" aria-controls="slick-slide00" aria-label="1 of 2" aria-selected="true">1</button></li>
+                     <li role="presentation">
+                        <button type="button" role="tab" id="slick-slide-control01" aria-controls="slick-slide03" aria-label="2 of 2" tabindex="-1">2</button></li>
+                  </ul>
+               </div>
 
 <?php include('footer.php') ?> 
