cbeeeb4ac122204a569d6536041a98b9a63bc2c3
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-12-03 14:36:27 +0800

    Add limit to font size increase

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,9 +168,21 @@ $( document ).ready(function() {
   
   $("#big").on("click",function(){
    
-    size+=4;
-     $(".main h1, .main h2, .main h3, .main h4, .main p, .main a, .main li, footer h1, footer h2, footer h3, footer h4, footer p, footer a, footer li").css("font-size",size + "px");
+
+
+     let sizeCounter = 0;
+     while (sizeCounter != 4){
+      size+=4;
+      $(".main h1, .main h2, .main h3, .main h4, .main p, .main a, .main li, footer h1, footer h2, footer h3, footer h4, footer p, footer a, footer li").css("font-size",size + "px");
+      sizeCounter++;
+     }
+     
   });
+
+
+
+
+
     
   $("#normal").on("click",function(){
     size=14;
