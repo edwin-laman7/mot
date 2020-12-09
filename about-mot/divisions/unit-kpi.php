fc71ea9a890d888ee21d5032ae6c3a2bb00cd910
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-12-09 17:12:53 +0800

    Create unit-kpi.php

---
+++ about-mot/divisions/unit-kpi.php
@@ -0,0 +1,62 @@
+<?php 
+//include('../header.php'); //parent content 
+include('../../header.php'); //sub content 
+?>
+
+<main class="boxed">
+<div class="grid-container">
+	<div class="row"> 
+		<div class="columns small-12 medium-8">
+<!-- do not copy above this line -->
+			<h2>Latar Belakang</h2>
+			<p>Unit KPI di Kementerian Pengangkutan telah diwujudkan pada tahun 2010. Unit ini pada asalnya adalah di bawah Bahagian Perancangan dan Penyelidikan, kemudian ditukar ke bawah Bahagian Parlimen dan Kabinet (BPdK) mulai pada bulan Ogos tahun 2013. Bagaimanapun, Jabatan Perkhidmatan Awam telah bersetuju dengan cadangan struktur baru Kementerian Pengangkutan melalui waran perjawatan tahun 2015 berkuatkuasa pada tarikh 16 Januari 2015 di mana Unit KPI telah diletakkan di bawah Ketua Setiausaha Kementerian. Melalui waran perjawatan yang telah dikeluarkan oleh JPA jumlah kakitangan Unit ini adalah seramai 2 orang sahaja iaitu Pegawai KPI Gred M52 dan Penolong Pegawai KPI Gred N36.</p>
+
+			<h2>Piagam Pelanggan</h2>
+			<p>Kami komited untuk memastikan:</p>
+			<ul>
+				<li>Visi Kementerian dicapai dengan mengenalpasti petunjuk prestasi utama dan sasaran berkaitan dilaksanakan mengikut tatacara pengurusan kewangan yang betul dan tahap integriti yang tinggi.</li>
+				<li>Petunjuk Prestasi Utama Jabatan / Agensi / Bahagian di bawah naungan Kementerian sentiasa ‘above and beyond’ ekspektasi Rakyat dengan pelaksanaan idea serta program yang mengambil pendekatan yang bersifat rasional dan bertanggungjawab bagi menunaikan amanah yang telah dipertanggungjawabkan.</li>
+				<li>Laporan pencapaian KPI yang ditetapkan dibuat dengan sistematik dan segera bagi memastikan Kementerian sentiasa berada di hala tuju yang betul.</li>
+				<li>Memantau serta melaporkan pencapaian KPI Kementerian kepada Mesyuarat Jawatankuasa KPI Kementerian Pengangkutan pada setiap suku tahun.</li>
+			</ul>
+
+			<h2>Fungsi</h2>
+			<p>Fungsi utama unit ini ialah sebagai focal point Kementerian di dalam melaksana dan memberi khidmat nasihat dalam urusan pembangunan Minister Performance Indicator (MPI) YB Menteri Pengangkutan dan Petunjuk Prestasi Utama (KPI) Penjawat Pengurusan Tertinggi Perkhidmatan Awam (PTPA) Tier 1 dan Tier 2. </p>
+			<p>Unit ini perlu memastikan pengurusan KPI Kementerian bersistematik dan komprehensif atas penyelarasan bersama ketua-ketua jabatan / agensi / bahagian / unit di bawah kementerian.</p>
+
+			<h2>Objektif</h2>
+			<ul>
+				<li>Memantapkan imej Kementerian melalui Pengurusan KPI bagi menghasilkan penetapan KPI yang lebih tepat, sistematik dan komprehensif.</li>
+				<li>Menyelaras dan memantau  pencapaian KPI Kementerian melalui hubungan dan kerjasama yang baik dengan semua focal person KPI Jabatan/Agensi/Bahagian.</li>
+				<li>Memastikan Pencapaian MPI / KPI Kementerian dikemaskini dan dilaporkan melalui sistem yang telah ditetapkan oleh Sekreteriat MPI, JPM dan JPA.</li>
+				<li>Bertanggungjawab dalam  urusan semakan MPI Kementerian bagi memastikan pencapaian MPI melambangkan pembangunan dan peningkatan sektor pengangkutan.</li>
+				<li>Memberi khidmat nasihat kepada agensi berkaitan aspek pembangunan KPI bagi memastikan KPI Kementerian dilaksanakan dengan teratur mengikut dasar dan prosedur yang ditetapkan.</li>
+			</ul>
+
+			<h2>Carta Organisasi</h2>
+			<p>**Organisation Chart Here**</p>
+
+			<h2>Informasi KPI</h2>
+			<p>MPI bagi tahun 2020 dicadangkan merangkumi butiran seperti berikut:</p>
+			<ol type="i">
+				<li><strong>Dua Teras</strong> iaitu:
+					<ol type="a">
+						<li>Tr 1 - Peningkatan Keluaran Dalam Negara Kasar (KDNKK) / pendapatan negara melalui sektor pengangkutan.</li>
+						<li>Tr 2- Perkhidmatan pengangkutan yang selamat, terjamin dan berkesan.</li>
+					</ol>
+				</li>
+				<li><strong>Enam Program</strong> di bawah dua Teras tersebut.</li>
+				<li>Keseluruhan Projek di bawah Program adalah berjumlah <strong>11 Projek.</strong></li>
+				<li>Jumlah keseluruhan Aktiviti di bawah Projek adalah sebanyak <strong>24 Aktiviti</strong></li>
+			</ol>
+<!-- do not copy BELOW this line -->
+		</div>
+		<div class="columns small-12 medium-4">
+			<?php include('division-sidebar.php')?>
+		</div>
+	</div>
+ </div>
+</main>
+<?php 
+//include('../footer.php'); //parent content
+include('../../footer.php'); //sub content ?> 
