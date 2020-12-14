837be2146cf6b11831f780003f1f14ae8d3bee9d
admLaman7 <72488556+admLaman7@users.noreply.github.com>
2020-12-14 09:34:48 +0800

    Create kpi-unit.php

---
+++ about-mot/divisions/kpi-unit.php
@@ -0,0 +1,62 @@
+<?php 
+//include('../header.php'); //parent content 
+include('../../header.php'); //sub content 
+?>
+
+<main class="boxed">
+<div class="grid-container">
+	<div class="row"> 
+		<div class="columns small-12 medium-8">
+<!-- do not copy above this line -->
+			<h2>Background</h2>
+			<p>The KPI Unit in the Ministry of Transport was established in 2010. The unit was originally under the Planning and Research Division, then changed under the Parliament and Cabinet Division (BPdK) from August 2013. However, the Public Service Department agreed to the proposed new structure of the Ministry of Transport through a 2015 employment warrant with effect from 16 January 2015 whereby KPI Unit were placed under the Secretary General of the Ministry. By employment warrant issued by PSD the total number of staff of this Unit is 2 persons, namely KPI Officer Grade M52 and Assistant KPI Officer Grade N36.</p>
+
+			<h2>Client's Charter</h2>
+			<p>We are committed to ensuring:</p>
+			<ul>
+				<li>The Ministry's vision is achieved by identifying key performance indicators and related targets implemented in accordance with proper financial management procedures and high level of integrity.</li>
+				<li>Key Performance Indicators Departments / Agencies / Divisions under the auspices of the Ministry are always 'above and beyond' the people's expedition with the implementation of ideas and programmes that take a rational and responsible approach to fulfill the trust that has been entrusted.</li>
+				<li>Regular KPI achievement reports are made systematically and promptly to ensure that the Ministry is in the right direction.</li>
+				<li>Monitor and report on the achievements of the Ministry's KPIs to the Ministry of Transport's KPI Committee Meetings on a quarterly basis.</li>
+			</ul>
+
+			<h2>Functions</h2>
+			<p>The main functions of this unit are as focal points of the Ministry in implementing and providing advisory services in the development of Minister Performance Indicator (MPI) YB Minister of Transport and Key Performance Indicators (KPI) Top Management Personnel Public Service (PTPA) Tier 1 and Tier 2.</p>
+			<p>This unit should ensure the management of Ministry’s KPI is systematic and comprehensive through coordination with heads of departments / agencies / divisions / units under the ministry.</p>
+
+			<h2>Objectives</h2>
+			<ul>
+				<li>Strengthen the Ministry's image through KPI Management to produce a more accurate, systematic and comprehensive KPI designation.</li>
+				<li>Coordinate and monitor the achievements of KPI Ministries through good relationships and cooperation with all focal persons KPI Department/Agency/Division.</li>
+				<li>Ensure that the achievements of the MPI / KPI of the Ministry are updated and reported through the systems set by the MPI, JPM and PSD Secretariat.</li>
+				<li>Responsible for the revision of MPI ministries to ensure the achievement of MPI symbolises the development and improvement of the transport sector.</li>
+				<li>To provide advisory services to agencies on the aspects of KPI development to ensure that the Ministry's KPI is carried out in a proper manner in accordance with established policies and procedures.</li>
+			</ul>
+
+			<h2>Organisation Chart</h2>
+			<p>**Organisation Chart Here**</p>
+
+			<h2>KPI Information</h2>
+			<p>The MPI for 2020 is proposed to include the following details: </p>
+			<ol type="i">
+				<li>Two Cores namely:
+					<ol type="a">
+						<li>Tr 1 - Increased Gross Domestic Product (GDP) / national income through the transportation sector</li>
+						<li>Tr 2 - Safe, secure and effective transportation services.</li>
+					</ol>
+				</li>
+				<li>Six Programmes under the two Cores.</li>
+				<li>The whole Project under the Programme amounts to 11 Project.</li>
+				<li>The total number of activities under the Project is 24 Activities.</li>
+			</ol>
+<!-- do not copy BELOW this line -->
+		</div>
+		<div class="columns small-12 medium-4">
+			<?php include('division-sidebar.php')?>
+		</div>
+	</div>
+ </div>
+</main>
+<?php 
+//include('../footer.php'); //parent content
+include('../../footer.php'); //sub content ?> 
